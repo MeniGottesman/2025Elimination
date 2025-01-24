e95a95d6b2288a2d07fb69b65b1863e1831ce628
MeniGottesman <MeniMangot@gmail.com>
2025-01-24 13:50:35 -0800

    footer edit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,18 +1,20 @@
-<footer class="fixed bottom-0 w-screen">
-	<div class="mx-auto w-full max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-		<span class="text-sm text-gray-400 sm:text-center"
-			>Made by <a href="https://github.com/ThePickleGawd" class="hover:underline">Dylan Lu</a>, improved by <a href="https://menigottesman.com/" class="hover:underline">Meni Gottesman</a></span>
->
-		<ul class="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-400 md:mt-0">
-			<li>
-				<a href="/" class="mr-4 hover:underline md:mr-6">Home</a>
-			</li>
-			<li>
-				<a href="/about" class="mr-4 hover:underline md:mr-6">About</a>
-			</li>
-			<li>
-				<a href="/contact" class="hover:underline">Contact</a>
-			</li>
-		</ul>
-	</div>
+<footer class="fixed bottom-0 w-full bg-gray-800 text-white">
+  <div class="mx-auto w-full max-w-screen-xl p-4 flex flex-col md:flex-row md:items-center md:justify-between">
+    <span class="text-sm text-gray-400 sm:text-center">
+      Made by 
+      <a href="https://github.com/ThePickleGawd" class="hover:underline">Dylan Lu</a>, improved by 
+      <a href="https://menigottesman.com/" class="hover:underline">Meni Gottesman</a>
+    </span>
+    <ul class="mt-3 flex flex-wrap items-center text-sm font-medium text-gray-400 md:mt-0">
+      <li>
+        <a href="/" class="mr-4 hover:underline md:mr-6">Home</a>
+      </li>
+      <li>
+        <a href="/about" class="mr-4 hover:underline md:mr-6">About</a>
+      </li>
+      <li>
+        <a href="/contact" class="hover:underline">Contact</a>
+      </li>
+    </ul>
+  </div>
 </footer>
