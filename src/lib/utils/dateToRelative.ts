6a8e2d23abdf76f753c44b7f9595d5651e12ac02
MeniGottesman <MeniMangot@gmail.com>
2025-01-28 19:43:57 -0800

    Fixed Leadership

--- src/lib/utils/dateToRelative.ts
+++ src/lib/utils/dateToRelative.ts
@@ -1,7 +1,13 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+// Extend the necessary plugins
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const dateToRelative = (date: string) => {
-	dayjs.extend(relativeTime);
-	return dayjs(date).fromNow();
+	return dayjs.utc(date).tz('America/Los_Angeles').fromNow();
 };
